9b8068601c7fb32a0ee32f936c95000e19e86399
Faizan <l1s22bsse0114@ucp.edu.pk>
2025-06-24 08:43:06 +0500

    modify setup sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 echo "Setting Up The New Repo"
 git init
 git branch -m develop
-git config --local user.name "Imran Ashraf"
-git config --local user.email "zsaing.ucp@gmail.com"
+git config --local user.name "Faizan Javed"
+git config --local user.email "l1s22bsse0114@ucp.edu.pk"
 git config --local core.editor notepad
 mkdir ./src ./styles ./assests ./bin ./node_modules
 touch ./src/index.html ./styles/index.css ./.gitignore
-git remote add origin https://github.com/DevOps-S25/s1week11s25.git
+git remote add origin https://github.com/FaizanxxJ/s1week11s25-fork-.git
 echo "New Repo Set Up Complete"
